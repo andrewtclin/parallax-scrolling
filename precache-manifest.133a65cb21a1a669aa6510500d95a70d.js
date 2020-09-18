782536dd86c64210ea2984f90f729c21ced426d2
Andrew <chuntcdj@gmail.com>
2020-09-18 11:45:14 +0800

    Updates

---
+++ precache-manifest.133a65cb21a1a669aa6510500d95a70d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff66aec5fa1a87c2e55a5e51df91b64",
+    "url": "/parallax-scrolling/index.html"
+  },
+  {
+    "revision": "dcdb8a097d2d770e13f0",
+    "url": "/parallax-scrolling/static/css/main.160debfa.chunk.css"
+  },
+  {
+    "revision": "46f085d6607720c9b42d",
+    "url": "/parallax-scrolling/static/js/2.7300592f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/parallax-scrolling/static/js/2.7300592f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcdb8a097d2d770e13f0",
+    "url": "/parallax-scrolling/static/js/main.6417b18d.chunk.js"
+  },
+  {
+    "revision": "70f2fd00f035dcf6233b",
+    "url": "/parallax-scrolling/static/js/runtime-main.902e83be.js"
+  }
+]);
